3aac4f3b5be17fbf7627f16a935cc701a51d0417
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-17 16:52:48 +0000

    Marshall item

--- src/services/spaces/PostSpaces.ts
+++ src/services/spaces/PostSpaces.ts
@@ -1,4 +1,5 @@
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { v4 } from 'uuid';
 
@@ -12,14 +13,8 @@ export async function postSpaces(
   const result = await ddbClient.send(
     new PutItemCommand({
       TableName: process.env.TABLE_NAME,
-      Item: {
-        id: {
-          S: randomId,
-        },
-        location: {
-          S: item.location,
-        },
-      },
+
+      Item: marshall(item),
     })
   );
   console.log(result);
