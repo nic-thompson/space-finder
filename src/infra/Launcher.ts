1f67230697e838e9f0d289cbc7fbe5aafa6c88c6
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-20 19:13:20 +0000

    Adds AuthStack

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -2,6 +2,7 @@ import { App } from 'aws-cdk-lib';
 import { ApiStack } from './stacks/ApiStack';
 import { DataStack } from './stacks/DataStack';
 import { LambdaStack } from './stacks/LambdaStack';
+import { AuthStack } from './stacks/AuthStack';
 
 const app = new App();
 
@@ -9,6 +10,7 @@ const dataStack = new DataStack(app, 'DataStack');
 const lambdaStack = new LambdaStack(app, 'LambdaStack', {
   spacesTable: dataStack.spacesTable,
 });
+new AuthStack(app, 'AuthStack');
 new ApiStack(app, 'ApiStack', {
   spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
 });
