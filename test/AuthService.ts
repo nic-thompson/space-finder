89fceaa140ff83cc39377f98b8332a3e3e69fee8
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-23 10:51:34 +0000

    Generates temporary credentials

--- test/AuthService.ts
+++ test/AuthService.ts
@@ -1,5 +1,7 @@
 import { type CognitoUser } from '@aws-amplify/auth';
 import { Amplify, Auth } from 'aws-amplify';
+import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
 
 const awsRegion = 'eu-west-2';
 
@@ -8,6 +10,7 @@ Amplify.configure({
     region: awsRegion,
     userPoolId: 'eu-west-2_DGZNO0od8',
     userPoolWebClientId: '1ah42dnj7j0ii1iadsh6sat1v1',
+    identityPoolId: 'eu-west-2:de58c2c8-0f6f-4e44-b01d-ed57a1b3bc5a',
     authenticationFlowType: 'USER_PASSWORD_AUTH',
   },
 });
@@ -16,5 +19,18 @@ export class AuthService {
   public async login(userName: string, password: string) {
     const result = (await Auth.signIn(userName, password)) as CognitoUser;
     return result;
+  }
+
+  public async generateTemporaryCredentials(user: CognitoUser) {
+    const jwtToken = user.getSignInUserSession().getIdToken().getJwtToken();
+    const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/eu-west-2_DGZNO0od8`;
+    const cognitoIdentity = new CognitoIdentityClient({
+      credentials: fromCognitoIdentityPool({
+        identityPoolId: 'eu-west-2:de58c2c8-0f6f-4e44-b01d-ed57a1b3bc5a',
+        logins: { [cognitoIdentityPool]: jwtToken },
+      }),
+    });
+    const credentials = await cognitoIdentity.config.credentials();
+    return credentials;
   }
 }
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -3,7 +3,9 @@ import { AuthService } from './AuthService';
 async function testAuth() {
   const service = new AuthService();
   const loginResult = await service.login('barosanu', 'OdOj8pog)');
-  console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+  // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+  const credentials = await service.generateTemporaryCredentials(loginResult);
+  console.log(credentials);
 }
 
 testAuth();
