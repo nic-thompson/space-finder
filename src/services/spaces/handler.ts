e180a01b1ca96685f955b8bf8878313b8ae2461f
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-24 12:17:16 +0000

    Adds CORS headers

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -9,6 +9,7 @@ import { getSpaces } from './GetSpaces';
 import { updateSpace } from './UpdateSpace';
 import { deleteSpace } from './DeleteSpace';
 import { JsonError, MissingFieldError } from '../shared/Validator';
+import { addCorsHeader } from '../shared/Utils';
 
 const ddbClient = new DynamoDBClient({});
 
@@ -16,25 +17,23 @@ async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  let message: string;
+  let response: APIGatewayProxyResult;
 
   try {
     switch (event.httpMethod) {
       case 'GET':
         const getResponse = await getSpaces(event, ddbClient);
-        console.log(getResponse);
-        return getResponse;
+        response = getResponse;
       case 'POST':
         const postResponse = await postSpaces(event, ddbClient);
-        return postResponse;
+        response = postResponse;
       case 'PUT':
         const putResponse = await updateSpace(event, ddbClient);
-        console.log(putResponse);
-        return putResponse;
+        response = putResponse;
       case 'DELETE':
         const deleteResponse = await deleteSpace(event, ddbClient);
         console.log(deleteResponse);
-        return deleteResponse;
+        response = deleteResponse;
       default:
         break;
     }
@@ -56,12 +55,7 @@ async function handler(
       body: JSON.stringify(error.message),
     };
   }
-
-  const response: APIGatewayProxyResult = {
-    statusCode: 200,
-    body: JSON.stringify(message),
-  };
-
+  addCorsHeader(response);
   return response;
 }
 
