2c141d270f88d45daeac48e79ef7407ce7092d1f
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-28 19:50:09 +0000

    Adds navigation bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName} />
+          <Outlet />
+        </>
+      ),
+      children: [
+        { path: '/', element: <div>Home</div> },
+        { path: '/login', element: <div>Login</div> },
+        { path: '/profile', element: <div>Profile</div> },
+        { path: '/spaces', element: <div>Spaces</div> },
+        { path: '/createSpaces', element: <div>Create Spaces</div> },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <h1>App works...</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
