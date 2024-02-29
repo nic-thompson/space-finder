98164c942b61d2bb7f6de830a2d1f77092d89f60
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-29 21:31:13 +0000

    Generates temporary credentials

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,6 +1,11 @@
 import { type CognitoUser } from '@aws-amplify/auth';
 import { Amplify, Auth } from 'aws-amplify';
 import { AuthStack } from '../../../outputs.json';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
+import {
+  CognitoIdentity,
+  CognitoIdentityClient,
+} from '@aws-sdk/client-cognito-identity';
 
 const awsRegion = 'eu-west-2';
 
@@ -17,12 +22,19 @@ Amplify.configure({
 
 export class AuthService {
   private user: CognitoUser | undefined;
+  private jwtToken: string | undefined;
+  private temporaryCredentials: object | undefined;
+
   public async login(
     userName: string,
     password: string
   ): Promise<Object | undefined> {
     try {
       this.user = (await Auth.signIn(userName, password)) as CognitoUser;
+      this.jwtToken = this.user
+        ?.getSignInUserSession()
+        ?.getIdToken()
+        .getJwtToken();
       return this.user;
     } catch (error) {
       console.error(error);
@@ -30,7 +42,32 @@ export class AuthService {
     }
   }
 
+  public async getTemporaryCredentials() {
+    if (this.temporaryCredentials) {
+      return this.temporaryCredentials;
+    }
+    this.temporaryCredentials = await this.generateTemporaryCredentials();
+    return this.temporaryCredentials;
+  }
+
   public getUserName() {
     return this.user?.getUsername();
+  }
+
+  private async generateTemporaryCredentials() {
+    const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/${AuthStack.SpaceUserPoolId}`;
+    const cognitoIdentity = new CognitoIdentityClient({
+      credentials: fromCognitoIdentityPool({
+        clientConfig: {
+          region: awsRegion,
+        },
+        identityPoolId: AuthStack.SpaceIdentityPoolId,
+        logins: {
+          [cognitoIdentityPool]: this.jwtToken!,
+        },
+      }),
+    });
+    const credentials = await cognitoIdentity.config.credentials();
+    return credentials;
   }
 }
