72a44f3177d76609031bc86382fa17285d029ce5
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-29 17:27:03 +0000

    Configures amplify to login in using cognito

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,14 +1,36 @@
+import { type CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
+import { AuthStack } from '../../../outputs.json';
+
+const awsRegion = 'eu-west-2';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: awsRegion,
+    userPoolId: AuthStack.SpaceUserPoolId,
+    userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
+    identityPoolId: AuthStack.SpaceIdentityPoolId,
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+  },
+});
+
 export class AuthService {
+  private user: CognitoUser | undefined;
   public async login(
     userName: string,
     password: string
   ): Promise<Object | undefined> {
-    return {
-      userName: 'userName',
-    };
+    try {
+      this.user = (await Auth.signIn(userName, password)) as CognitoUser;
+      return this.user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 
   public getUserName() {
-    return 'userName';
+    return this.user?.getUsername();
   }
 }
